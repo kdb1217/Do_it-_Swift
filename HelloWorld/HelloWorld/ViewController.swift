f8d917b8b2f33affe4b39ac1c5d064902c3e3104
김다빈 <kdb1217@gimdabin-ui-noteubug.local>
2023-03-13 16:03:36 +0900

    Do_it 61p

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 김다빈 on 2023/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+    }
+    
+}
+
